63b23a3e949c046e00c2373038ab4f078c130ae0
Xin Zhang <54650953+xinnnix@users.noreply.github.com>
2022-12-30 18:06:48 +0800

    update

---
+++ Learning_Python/classscope.py
@@ -0,0 +1,20 @@
+X = 1
+
+def nester():
+    print(X)
+    
+    class C:
+        print(X)
+        def method1(self):
+            print(X)
+        def method2(self):
+            X = 3
+            print(X)
+            
+    I = C()
+    I.method1()
+    I.method2()
+    
+print(X)
+nester()
+print('-'*40)
